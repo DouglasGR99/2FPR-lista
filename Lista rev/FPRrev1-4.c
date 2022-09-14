5d2c69d3fcb91a7f38eae5a8336ee7d0269d9a89
Douglas Garcia <douglasgovmad@gmail.com>
2022-09-13 23:54:05 -0300

    indentation corection lista 1-4

--- Lista rev/FPRrev1-4.c
+++ Lista rev/FPRrev1-4.c
@@ -10,7 +10,8 @@
 
 int mdc(int n1,int n2);
 
-void main(){
+void main()
+{
 	int n1,n2;
 	printf("Insira n1 e n2:\n");
 	scanf("%d%d",&n1,&n2);
@@ -25,15 +26,18 @@ int mdc(int n1,int n2){
 	der resto = 0 sera o mdc.
 */
 	
-	for(i=n1;i>=1;i--){
+	for(i=n1;i>=1;i--)
+	{
 		
 		res1=n1%i;
 		printf("\ni=%d",i);
-		for(j=n2;j>=1;j--){
+		for(j=n2;j>=1;j--)
+		{
 			
 			res2=n2%j;
 			printf(" j=%d",j);
-			if(res1==0&&res2==0&&j==i){
+			if(res1==0&&res2==0&&j==i)
+			{
 				return j;
 			}
 		}
